f192c9e9bb1cd1fbdb36f2e1cbd9351abaff8983
cho-k <52272347+cho-k@users.noreply.github.com>
2019-06-27 16:53:18 +0900

    Create calendar_confirm.php

---
+++ AttendanceManagementSystem/calendar_confirm.php
@@ -0,0 +1,34 @@
+<?
+require('lib/configuration.lib');
+require('lib/core.lib');
+
+
+	session_start();
+
+	//get_param($param);
+
+	// データベース接続
+	$dbcon = database_connect();
+
+
+	// ---------------
+	// データ取得
+	// ---------------
+
+	$num_of_holiday = get_holiday($holiday);
+	$HOLIDAY = array();
+	for($i=0; $i<$num_of_holiday; $i++) {
+		$HOLIDAY += array($holiday[$i]['holiday_day'] => $holiday[$i]['holiday_name']);
+	}
+  
+
+	// ---------------
+	// 画面表示
+	// ---------------
+
+	header('Content-Type: text/html; charset=utf-8');
+	include('temp/calendar_confirm.html');
+
+	database_close();
+	exit;
+?>
